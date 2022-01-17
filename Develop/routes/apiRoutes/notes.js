3826effc4269bd0e41a40d7884bd491e1a04c44c
dmknapp2385 <dmknapp2385@gmail.com>
2022-01-16 17:33:32 -0700

    get and post note api routes

--- Develop/routes/apiRoutes/notes.js
+++ Develop/routes/apiRoutes/notes.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
 const path = require('path');
+const notes = require ('../../db/db.json');
+const fs = require('fs');
 
+router.get('/notes', (res, req) => {
+    let notes = notes;
+    res.json(notes);
+});
 
+router.post('/notes', (res,req) => {
+    let note = req.body;
+    //function with outside npm library to create unique id for each note
+    notes.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify({ notes }, null, 2)
+  );
+  return note;  
+});
 
 module.exports = router;
