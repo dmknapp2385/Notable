81d20747a9eb5397cacc5bd8a84e8ea831074334
dmknapp2385 <dmknapp2385@gmail.com>
2022-01-18 14:22:42 -0700

    restructured folders

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -4,13 +4,15 @@ const notes = require ('../../db/db.json');
 const fs = require('fs');
 
 router.get('/notes', (req, res) => {
-    let notes = notes;
-    res.json(notes);
+    let savedNotes = notes;
+    res.json(savedNotes);
 });
 
 router.post('/notes', (req, res) => {
     let note = req.body;
-    //function with outside npm library to create unique id for each note
+    alert('note saved');
+    console.log(note);
+        //function with outside npm library to create unique id for each note
     notes.push(note);
     fs.writeFileSync(
         path.join(__dirname, '../../db/db.json'),
