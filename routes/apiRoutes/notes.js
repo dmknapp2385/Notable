98d7e0244cd5ba69c80959d2409090bae10dc45f
dmknapp2385 <dmknapp2385@gmail.com>
2022-01-18 15:53:28 -0700

    note post to json, need to fix render notes function

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,18 +1,17 @@
 const router = require('express').Router();
 const path = require('path');
-const notes = require ('../../db/db.json');
+const {notes} = require ('../../db/db.json');
 const fs = require('fs');
 
 router.get('/notes', (req, res) => {
     let savedNotes = notes;
-    res.json(savedNotes);
+    res.send(savedNotes);
 });
 
 router.post('/notes', (req, res) => {
     let note = req.body;
-    alert('note saved');
     console.log(note);
-        //function with outside npm library to create unique id for each note
+    console.log(notes);
     notes.push(note);
     fs.writeFileSync(
         path.join(__dirname, '../../db/db.json'),
